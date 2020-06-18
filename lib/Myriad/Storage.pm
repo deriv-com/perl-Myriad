929d768ff6842c290fb6ec373ff92e42a8bef326
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2020-06-18 14:01:48 +0100

    Initial attempt at providing timing and outcome metrics from Myriad::Storage methods

--- lib/Myriad/Storage.pm
+++ lib/Myriad/Storage.pm
@@ -15,6 +15,9 @@ use Role::Tiny;
 
 use experimental qw(signatures);
 
+use Metrics::Any qw($metrics);
+use Time::HiRes ();
+
 =encoding utf8
 
 =head1 NAME
@@ -51,6 +54,32 @@ a concrete implementation - instead, see classes such as:
 
 =cut
 
+$metrics->make_timer( elapsed =>
+    name        => [qw( myriad storage call elapsed )],
+    description => "Elapsed time spent processing storage requests",
+    # TODO: A shared category/service across the whole process?
+    labels      => [qw( method status )],
+);
+
+sub _wrap_method_for_metrics ($method) {
+    around $method => sub ($orig, $self, @args) {
+        my $start = [Time::HiRes::gettimeofday()];
+        return $self->$orig(@args)
+            ->then_with_f( sub {
+                my ($f) = @_;
+                $metrics->report_timer( elapsed => Time::HiRes::tv_interval($start),
+                    { method => $method, status => "success" } );
+                $f;
+            })
+            ->else_with_f( sub {
+                my ($f) = @_;
+                $metrics->report_timer( elapsed => Time::HiRes::tv_interval($start),
+                    { method => $method, status => "failure" } );
+                $f;
+            });
+        };
+}
+
 =head2 get
 
 Takes the following parameters:
@@ -66,6 +95,8 @@ Returns a L<Future> which will resolve to the corresponding value, or C<undef> i
 =cut
 
 requires 'get';
+
+_wrap_method_for_metrics 'get';
 
 =head2 set
 
@@ -87,6 +118,8 @@ Returns a L<Future> which will resolve on completion.
 =cut
 
 requires 'set';
+
+_wrap_method_for_metrics 'set';
 
 =head2 observe
 
@@ -116,6 +149,8 @@ Returns a L<Future> which will resolve to .
 
 requires 'push';
 
+_wrap_method_for_metrics 'push';
+
 =head2 unshift
 
 Takes the following parameters:
@@ -131,6 +166,8 @@ Returns a L<Future> which will resolve to .
 =cut
 
 requires 'unshift';
+
+_wrap_method_for_metrics 'unshift';
 
 =head2 pop
 
@@ -148,6 +185,8 @@ Returns a L<Future> which will resolve to .
 
 requires 'pop';
 
+_wrap_method_for_metrics 'pop';
+
 =head2 shift
 
 Takes the following parameters:
@@ -163,6 +202,8 @@ Returns a L<Future> which will resolve to .
 =cut
 
 requires 'shift';
+
+_wrap_method_for_metrics 'shift';
 
 =head2 hash_set
 
@@ -180,6 +221,8 @@ Returns a L<Future> which will resolve to .
 
 requires 'hash_set';
 
+_wrap_method_for_metrics 'hash_set';
+
 =head2 hash_get
 
 Takes the following parameters:
@@ -195,6 +238,8 @@ Returns a L<Future> which will resolve to the scalar value for this key.
 =cut
 
 requires 'hash_get';
+
+_wrap_method_for_metrics 'hash_get';
 
 =head2 hash_add
 
@@ -212,6 +257,8 @@ Returns a L<Future> indicating success or failure.
 
 requires 'hash_add';
 
+_wrap_method_for_metrics 'hash_add';
+
 =head2 hash_keys
 
 Takes the following parameters:
@@ -227,6 +274,8 @@ Returns a L<Future> which will resolve to a list of the keys in no defined order
 =cut
 
 requires 'hash_keys';
+
+_wrap_method_for_metrics 'hash_keys';
 
 =head2 hash_values
 
@@ -244,6 +293,8 @@ Returns a L<Future> which will resolve to a list of the values in no defined ord
 
 requires 'hash_values';
 
+_wrap_method_for_metrics 'hash_values';
+
 =head2 hash_exists
 
 Takes the following parameters:
@@ -259,6 +310,8 @@ Returns a L<Future> which will resolve to true if the key exists in this hash.
 =cut
 
 requires 'hash_exists';
+
+_wrap_method_for_metrics 'hash_exists';
 
 =head2 hash_count
 
@@ -276,6 +329,8 @@ Returns a L<Future> which will resolve to the count of the keys in this hash.
 
 requires 'hash_count';
 
+_wrap_method_for_metrics 'hash_count';
+
 =head2 hash_as_list
 
 Takes the following parameters:
@@ -292,6 +347,8 @@ suitable for assigning to a hash.
 =cut
 
 requires 'hash_as_list';
+
+_wrap_method_for_metrics 'hash_as_list';
 
 1;
 
