3aeafddf096a218a2aa7777a2944dcac99eeb4b1
Tom Molesworth <tom@binary.com>
2020-06-22 12:35:18 +0800

    Fix compilation for method -> sub conversion

--- lib/Myriad/RPC/Implementation/Redis.pm
+++ lib/Myriad/RPC/Implementation/Redis.pm
@@ -14,99 +14,94 @@ use experimental qw(signatures);
 
 use Future::AsyncAwait;
 use Syntax::Keyword::Try;
+use Sys::Hostname qw(hostname);
 use Role::Tiny::With;
+use Scalar::Util qw(blessed);
 
 use Log::Any qw($log);
-use Sys::Hostname;
 
 with 'Myriad::RPC';
 
-has $redis;
-has $group_name;
-has $whoami;
-has $service;
-has $rpc_map;
+sub rpc_map { shift->rpc_map }
 
-method rpc_map :lvalue { $rpc_map }
-
-method BUILD(%args) {
-    $whoami = hostname;
-    $group_name = 'processors';
+sub configure ($self, %args) {
+    for my $k (qw(whoami group_name redis service)) {
+        $self->{$k} = delete $args{$k} if exists $args{$k};
+    }
+    $self->{whoami} //= hostname();
+    $self->{group_name} //= 'processors';
 }
 
-method configure(%args) {
-    $redis = delete $args{redis} if exists $args{redis};
-    $service = delete $args{service} if exists $args{service};
+sub _add_to_loop ($self, $loop) {
+    $self->listen->retain;
 }
 
-method _add_to_loop($loop) {
-    $self->listen()->retain();
-}
-
-async method listen() {
-    await $redis->create_group($service, $group_name);
-    my $stream_config = { stream => $service, group => $group_name, client => $whoami };
-    my $pending_requests = $redis->pending(%$stream_config);
-    my $incoming_request = $redis->iterate(%$stream_config);
+async sub listen ($self) {
+    await $self->redis->create_group(
+        $self->service,
+        $self->group_name
+    );
+    my %stream_config = (
+        stream => $self->service,
+        group  => $self->group_name,
+        client => $self->whoami
+    );
+    my $pending_requests = $self->redis->pending(%stream_config);
+    my $incoming_request = $self->redis->iterate(%stream_config);
 
     try {
-        await $incoming_request->merge($pending_requests)->map(sub {
-            # Redis response is array ref we need a hashref
-            my %args = @$_;
-            return \%args;
-        })->map(sub {
-            my ($data) = @_;
-            try {
-                { message => Myriad::RPC::Message->new($data->%*) };
-            } catch {
-                my $error = $@;
-                if (!$@->isa('Myriad::Exception')) {
-                    $error = Myriad::Exception::InternalError->new();
+        await $incoming_request->merge($pending_requests)
+            ->map(sub {
+                my ($data) = @_;
+                try {
+                    { message => Myriad::RPC::Message->new(@$data) };
+                } catch {
+                    my $error = $@;
+                    $error = Myriad::Exception::InternalError->new($@) unless blessed($error) and $error->isa('Myriad::Exception');
+                    return { error => $error, id => $data->{message_id} }
                 }
-                return { error => $error, id => $data->{message_id} }
-            }
-        })->each(sub {
-            if (my $error = $_->{error}) {
-                $log->warnf("error while parsing the incoming messages: %s", $error->message);
-                $rpc_map->{__DEAD_MSG}->[0]->emit($_->{id});
-            } else {
-                my $message = $_->{message};
-                if (my $method = $rpc_map->{$message->rpc}) {
-                    $method->[0]->emit($message);
+            })->each(sub {
+                if (my $error = $_->{error}) {
+                    $log->warnf("error while parsing the incoming messages: %s", $error->message);
+                    $self->rpc_map->{__DEAD_MSG}->[0]->emit($_->{id});
                 } else {
-                    my $error = Myriad::Exception::RPCMethodNotFound->new(method => $method);
-                    $rpc_map->{'__ERROR'}->[0]->emit({message => $message, error => $error});
+                    my $message = $_->{message};
+                    if (my $sub = $self->rpc_map->{$message->rpc}) {
+                        $sub->[0]->emit($message);
+                    } else {
+                        my $error = Myriad::Exception::RPCMethodNotFound->new(sub => $sub);
+                        $self->rpc_map->{'__ERROR'}->[0]->emit({message => $message, error => $error});
+                    }
                 }
-            }
-        })->completed;
+            })->completed;
     } catch {
         $log->fatalf("RPC listener stopped due: %s", $@);
     }
 }
 
-async method _reply($message) {
+async sub _reply ($self, $message) {
     try {
-        await $redis->publish($message->who, $message->encode);
-        await $redis->ack($service, $group_name, $message->id);
+        await $self->redis->publish($message->who, $message->encode);
+        await $self->redis->ack($self->service, $self->group_name, $message->id);
     } catch {
         $log->warnf("Failed to reply to client due: %s", $@);
         return;
     }
 }
 
-async method reply_success($message, $response) {
+async sub reply_success ($self, $message, $response) {
     $message->response = { response => $response };
     await $self->_reply($message);
 }
 
-async method reply_error($message, $error) {
+async sub reply_error ($self, $message, $error) {
     $message->response = { error => { code => $error->category, message => $error->message } };
     await $self->_reply($message);
 }
 
-async method drop($id) {
+async sub drop ($self, $id) {
     $log->debugf("Going to drop message: %s", $id);
-    await $redis->ack($service, $group_name, $id);
+    await $self->redis->ack($self->service, $self->group_name, $id);
 }
 
 1;
