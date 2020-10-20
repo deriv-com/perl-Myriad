a331768375e0e6a6be3752a7ed47ec9a51f872a7
Eyad Arnabeh <eyadof@gmail.com>
2020-10-20 14:40:33 +0800

    move the RPC exceptions into Myriad::RPC

--- lib/Myriad/RPC.pm
+++ lib/Myriad/RPC.pm
@@ -24,6 +24,47 @@ Myriad::RPC - microservice RPC abstraction
 =cut
 
 use Role::Tiny;
+use Myriad::Exception::Builder;
+
+has $error_category = 'rpc';
+
+=head1 Exceptions
+
+=cut
+
+=head2 InvalidRequest
+
+Returned when there is issue parsing the request, or if the request parameters are incomplete.
+
+=cut
+
+declare_exception InvalidRequest => (
+    category => $error_category,
+    message => 'Invalid request'
+);
+
+=head2 MethodNotFound
+
+Returned if the requested method is not recognized by the service.
+
+=cut
+
+declare_exception MethodNotFound => (
+    category => $error_category,
+    message => 'Method not found'
+);
+
+=head2 Timeout
+
+Returned when there is an external timeout or the request deadline is already passed.
+
+=cut
+
+declare_exception Timeout => (
+    category => $error_category,
+    message => 'Timeout'
+);
+
 
 1;
 
