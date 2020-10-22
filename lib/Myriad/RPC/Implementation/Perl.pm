433eacfb96edba2be06db2add350b34af00ba6d8
Eyad Arnabeh <eyadof@gmail.com>
2020-10-22 15:48:35 +0800

    documentation & tests

--- lib/Myriad/RPC/Implementation/Perl.pm
+++ lib/Myriad/RPC/Implementation/Perl.pm
@@ -35,6 +35,14 @@ method configure(%args) {
     $self->next::method(%args);
 }
 
+=head1 METHODS
+
+=head2 start
+
+Start waiting for new requests to fill in the internal requests queue.
+
+=cut
+
 async method start () {
     $should_shutdown //= $self->loop->new_future(label => 'rpc::perl::shutdown_future')->without_cancel;
     my $id = 0;
@@ -61,6 +69,12 @@ async method start () {
     }
 }
 
+=head2 create_from_sink
+
+Register and RPC call and save a reference to its L<RYU::Sink>.
+
+=cut
+
 method create_from_sink (%args) {
     my $sink   = $args{sink} // die 'need a sink';
     my $method = $args{method} // die 'need a method name';
@@ -68,9 +82,23 @@ method create_from_sink (%args) {
     $rpc_methods->{$method} = $sink;
 }
 
+=head2 stop
+
+Gracefully stop the RPC processing.
+
+=cut
+
 async method stop () {
     $should_shutdown->done();
 }
+
+=head2 reply_success
+
+Reply to the requester with a success message.
+
+In this implementation it's done by resolving the L<Future> calling C<done>.
+
+=cut
 
 async method reply_success ($message, $response) {
     my $future = shift @pending_requests;
@@ -78,15 +106,39 @@ async method reply_success ($message, $response) {
     $future->done($message->encode);
 }
 
+=head2 reply_error
+
+Reply to the requester with a failure message.
+
+In this implementation it's done by resolving the L<Future> calling C<fail>.
+
+=cut
+
 async method reply_error ($message, $error) {
     my $future = shift @pending_requests;
     $message->response = { error => { category => $error->category, message => $error->message, reason => $error->reason } };
     $future->fail($message->encode);
 }
 
+=head2 drop
+
+Drop the request because we can't reply to the requester.
+
+=cut
+
 async method drop ($id) {
     shift @pending_requests;
 }
+
+=head2 request
+
+Create a new request.
+
+=item * C<message> - A string that typically should contain JSON encoded message.
+
+=item * C<reply_future> - A L<Future> that is going to be resolved once the request is processed.
+
+=cut
 
 method request ($message, $reply_future) {
     push @pending_requests, $reply_future;
--- t/RPC/perl.t
+++ t/RPC/perl.t
@@ -31,12 +31,13 @@ $loop->add(my $rpc = Myriad::RPC::Implementation::Perl->new());
 
 isa_ok($rpc, 'IO::Async::Notifier');
 
-(async sub {
+my $sink = $ryu->sink(label=> 'rpc::test');
 
-    my $sink = $ryu->sink(label=> 'rpc::test');
+$rpc->create_from_sink(method => 'test', sink => $sink);
+$rpc->start()->retain;
 
-    $rpc->create_from_sink(method => 'test', sink => $sink);
-    $rpc->start()->retain;
+subtest 'it should return method not found' => sub {
+    (async sub {
 
     my $response = $loop->new_future;
     $message_args->{rpc} = 'not_found';
@@ -48,9 +49,36 @@ isa_ok($rpc, 'IO::Async::Notifier');
         like($e, qr{Method not found}, '');
     }
 
-    $rpc->stop();
 
-})->()->get();
+    })->()->get();
+
+};
+
+
+subtest 'it should propagate the message correctly' => sub {
+    (async sub {
+        my $response = $loop->new_future;
+
+        $message_args->{rpc} = 'test';
+        $rpc->request($message_args, $response);
+
+        $sink->source->take(1)->each(sub {
+            my $message = shift;
+            $rpc->reply_success($message, {success => 1});
+        })->completed->retain();
+
+        my $reply = await $response;
+        ok($reply, 'request should be propagated to the sink');
+    })->()->get;
+};
+
+
+subtest 'it should shutdown cleanly' => sub {
+    (async sub {
+        my $f = await $rpc->stop;
+        ok($f, 'it should stop');
+    })->()->get();
+};
 
 done_testing;
 
