c90f2c237cd6cc33a1ad7e17aa418544688cee27
Nael Alolwani <nael@binary.com>
2020-11-18 10:58:55 +0800

    update example services

--- example/logic-demo.pl
+++ example/logic-demo.pl
@@ -1,61 +1,82 @@
-#!/usr/bin/env perl 
+#!/usr/bin/env perl
 use strict;
 use warnings;
 
 use Myriad;
 
 {
-package Example::Service::Holder;
+package Example::Service::Trigger;
 
-# Simple batch method example.
+# Simple service, has a value that sends an event on every update to it.
 
 use Myriad::Service;
 use Ryu::Source;
 
 has $count = 0;
-has $value = Ryu::Source->new;
+has $value;
+has $call_event_handler = Ryu::Source->new;
 
 async method current : RPC {
     return { value => $value, count => $count};
 }
 
-async method update_v : RPC (%args) {
+async method update : RPC (%args) {
 
-    $value->emit($args{new_value});
-    return 1;
+    $value = $args{new_value};
+    $count = 0 if $args{reset};
+    $call_event_handler->emit(1);
+    return await $self->current;
 
 }
 
-async method call_counter : Emitter() ($sink, $api, %args){
-    $value->each(sub {
-        my $v = shift;
-        my $e = {name => "EMITTER-Holder serv", value => $v, count => ++$count};
+
+async method value_updated : Emitter() ($sink, $api, %args){
+    $call_event_handler->each(sub {
+            #my ($v, $reset) = @_;
+            #$count = 0 if $reset;
+        my $e = {name => "EMITTER-Trigger service", value => $value, count => ++$count};
         $sink->emit($e);
     });
+
 }
 
 }
 
 {
-package Example::Service::Consumer;
+package Example::Service::Holder;
 
-# Simple batch method example.
+# Simple service, react on a received event by keeping the sum of its emitted values.
 
 use Myriad::Service;
+use JSON::MaybeUTF8 qw(:v1);
 
+has $sum = 0;
 has $count = 0;
 
-async method call_counter :Receiver(service => 'example.service.holder') ($sink, $api, %args) {
+async method value_updated :Receiver(service => 'example.service.trigger') ($sink, $api, %args) {
     $log->warnf('Receiver Called | %s | %s | %s');
 
     while(1) {
-        use Data::Dumper;
-        await $sink->map(sub {my $t = shift; $log->warnf('fff %s', Dumper($t)); $count = $t;})->completed;
+        await $sink->map(
+            sub {
+                my $e = shift;
+                my %info = ($e->@*);
+                $log->tracef('INFO %s', \%info);
+
+                my $data = decode_json_utf8($info{'data'});
+                if ( ++$count == $data->{count} ){
+                    $sum += $data->{value};
+                } else {
+                    $sum = $data->{value};
+                    $count = $data->{count};
+                }
+            })->completed;
     }
 
 }
-async method current_s : RPC {
-    return $count;
+
+async method current_sum : RPC {
+    return { sum => $sum, count => $count};
 }
 
 
@@ -69,31 +90,12 @@ use Test::More;
 
 (async sub {
     my $myriad = Myriad->new;
-=s
-    $myriad->add_service(
-        'Example::Service::Emitter',
-        name => 'example_service_emitter',
-    );
-    $myriad->add_service(
-        'Example::Service::Test',
-        name => 'example',
-    );
-=cut
-    my @arg = ("-l","debug","--redis_uri","redis://redis6:6379","Example::Service::Holder,Example::Service::Consumer");
+
+    my @arg = ("-l","debug","--redis_uri","redis://redis6:6379","Example::Service::Trigger,Example::Service::Holder");
     $myriad->configure_from_argv(@arg)->get;
     $log->warnf('done configuring');
     $myriad->run;
-    {
-        my $srv = $myriad->service_by_name('example.service.emitter');
-        my $tst_srv = $myriad->service_by_name('example');
-        is(await $srv->current, 1, '1 because it has been already called');
-        is(await $tst_srv->current_s, 0, '0 00000000');
-        # Defer one iteration on the event loop
-        await $myriad->loop->delay_future(after => 1);
-        use Data::Dumper;
-        note "ssssss " . Dumper($tst_srv->current_s);
-        is(await $srv->current, 10, 'and now we should have 10');
-    }
+
 })->()->get;
 
 done_testing();
