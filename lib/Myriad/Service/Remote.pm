923b46bb2ef3e63c837aa04beb860d6257fc2636
Nael Alolwani <nael@binary.com>
2021-04-23 08:13:32 +0800

    Myriad/fix subscription command (#108)
    * load subscription first, to allow it add Myriad->on_start then let it be used in command
    
    * empty
    
    * log message order
    
    * make storage command error visible
    
    * Simpler value-if-defined logic
    
    * No need to go through map since we can use a hash slice instead
    
    Co-authored-by: Nael Alolwani <naelalolwani@gmail.com>
    Co-authored-by: Tom Molesworth <tom@deriv.com>

--- lib/Myriad/Service/Remote.pm
+++ lib/Myriad/Service/Remote.pm
@@ -80,9 +80,9 @@ it subscribes to a channel in the remote service.
 
 =cut
 
-method subscribe ($channel, $client = "remote_service") {
+async method subscribe ($channel, $client = "remote_service") {
    my $sink = $myriad->ryu->sink;
-   $myriad->subscription->create_from_sink(
+   await $myriad->subscription->create_from_sink(
         sink => $sink,
         service => $service_name,
         client => $client,
