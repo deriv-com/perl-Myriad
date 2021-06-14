8e1658bdd587ebdca21c86b719fede09b326bca3
Eyad Arnabeh <eyadof@gmail.com>
2021-06-14 18:16:11 +0800

    Correct invocation with params

--- lib/Test/Myriad/Service.pm
+++ lib/Test/Myriad/Service.pm
@@ -55,7 +55,8 @@ BUILD (%args) {
                         return delete $mocked_rpc->{$method};
                     }
                     try {
-                        await $_[0]->$method(%_);
+                        my $self = shift;
+                        await $self->$method(@_);
                     } catch ($e) {
                         $log->tracef("An exception has been thrown while calling the original sub - %s", $e);
                         die $e;
