16772224ca5c76850b28d9d0d7d28002ccb80231
Tom Molesworth <tom@binary.com>
2021-07-22 04:53:03 +0800

    feature/object_pad_role (#195)
    * feature/object_pad_role
    
    * Include initial Myriad::Role module
    
    * Make class setup optional, in case we want to apply pragmata only
    
    * Support for Myriad::Class does => ...
    
    * Extend rôle handling, should be functionally complete
    
    * Include test case
    
    ... or segfault generator, depending on your perspective.
    
    * Mark `does` as TODO for now

--- lib/Myriad/Class.pm
+++ lib/Myriad/Class.pm
@@ -233,21 +233,6 @@ sub import {
     List::Keywords->import(qw(any all));
     Syntax::Keyword::Match->import(qw(match));
 
-    # For history here, see this:
-    # https://rt.cpan.org/Ticket/Display.html?id=132337
-    # At the time of writing, ->begin_class is undocumented
-    # but can be seen in action in this test:
-    # https://metacpan.org/source/PEVANS/Object-Pad-0.21/t/70mop-create-class.t#L30
-    Object::Pad->import_into($pkg);
-    my $meta = Object::Pad::MOP::Class->begin_class(
-        $pkg,
-        (
-            $args{extends}
-            ? (extends => $args{extends})
-            : ()
-        )
-    );
-
     {
         no strict 'refs';
         # Essentially the same as importing Log::Any qw($log) for now,
@@ -259,7 +244,26 @@ sub import {
         );
         *{$pkg . '::tracer'}  = \(OpenTracing->global_tracer);
     }
-    return $meta;
+
+    if(my $class = $args{class} || $pkg) {
+        # For history here, see this:
+        # https://rt.cpan.org/Ticket/Display.html?id=132337
+        # At the time of writing, ->begin_class is undocumented
+        # but can be seen in action in this test:
+        # https://metacpan.org/source/PEVANS/Object-Pad-0.21/t/70mop-create-class.t#L30
+        Object::Pad->import_into($pkg);
+        my $meta = Object::Pad::MOP::Class->begin_class(
+            $pkg,
+            (
+                $args{extends}
+                ? (extends => $args{extends})
+                : ()
+            ),
+        );
+        # Note that `does` is not supported yet due to https://rt.cpan.org/Ticket/Display.html?id=137952
+        return $meta;
+    }
+    return $pkg;
 }
 
 1;
