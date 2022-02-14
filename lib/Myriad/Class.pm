d022654c6836cd7a2a3acbd31f02273e30ae1368
Tom Molesworth <tom@binary.com>
2022-02-14 16:14:07 +0800

    Allow (type => "role") when defining a class

--- lib/Myriad/Class.pm
+++ lib/Myriad/Class.pm
@@ -250,11 +250,9 @@ sub import {
     if(my $class = $args{class} || $pkg) {
         # For history here, see this:
         # https://rt.cpan.org/Ticket/Display.html?id=132337
-        # At the time of writing, ->begin_class is undocumented
-        # but can be seen in action in this test:
-        # https://metacpan.org/source/PEVANS/Object-Pad-0.21/t/70mop-create-class.t#L30
         Object::Pad->import_into($pkg);
-        my $meta = Object::Pad::MOP::Class->begin_class(
+        my $method = 'begin_' . ($args{type} || 'class');
+        my $meta = Object::Pad::MOP::Class->$method(
             $pkg,
             (
                 $args{extends}
