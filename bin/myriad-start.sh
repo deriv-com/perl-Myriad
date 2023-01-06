5b68b6e393d3fbf7f2219d1243db97cc2aa9cc56
Nael Alolwani <nael@binary.com>
2023-01-06 15:43:16 +0000

    update myriad-start.sh script to pass signal to service process

--- bin/myriad-start.sh
+++ bin/myriad-start.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
+_term() {
+    kill -TERM "$myriad" 2>/dev/null
+    wait "$myriad"
+}
+
+_int() {
+    kill -INT "$myriad" 2>/dev/null
+    wait "$myriad"
+}
+
+_quit() {
+    kill -QUIT "$myriad" 2>/dev/null
+    wait "$myriad"
+}
+
+trap _term SIGTERM
+trap _int SIGINT
+trap _quit SIGQUIT
+
 if [ ! -z $MYRIAD_DEV ]
 then
-    myriad-dev.pl $@
-else
-    myriad.pl $@
+        myriad-dev.pl $@ &
+    else
+        myriad.pl $@ &
 fi
+
+myriad=$!
+wait "$myriad"
