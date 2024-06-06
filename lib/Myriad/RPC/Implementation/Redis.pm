332095bbcc1657e2949e9790c498b76209854e4e
Tom Molesworth <tom@deriv.com>
2024-06-06 14:37:53 +0800

    Helper function to reverse stream name encoding to service+method names

--- lib/Myriad/RPC/Implementation/Redis.pm
+++ lib/Myriad/RPC/Implementation/Redis.pm
@@ -53,6 +53,11 @@ sub stream_name_from_service ($service, $method) {
     return RPC_PREFIX . ".$service.". RPC_SUFFIX . "/$method"
 }
 
+sub service_from_stream_name ($stream_name) {
+    my ($service, $method) = $stream_name =~ m{\Q@{[RPC_PREFIX()]}\E\.([^/]+)\.\Q@{[RPC_SUFFIX]}\E/(.*)};
+    return ($service, $method);
+}
+
 method configure (%args) {
     $redis = delete $args{redis} if exists $args{redis};
     $whoami = hostname();
